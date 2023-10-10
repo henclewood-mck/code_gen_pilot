4b800be75a8701bb3d27f2226b87a36af56a7bae
Dwayne Henclewood <Dwayne_Henclewood@mckinsey.com>
2023-10-09 23:35:27 -0400

    updated  GenSoluiton_1

--- src/main/java/com/acme/basic/GenSolution_1.java
+++ src/main/java/com/acme/basic/GenSolution_1.java
@@ -2,14 +2,27 @@ package com.acme.basic;
 import java.util.*;
 import java.lang.*;
 
-public class GenSolution_0 {
-    public boolean hasCloseElements(List<Double> numbers, double threshold) {
-        for (int i = 0; i < numbers.size(); i++) {
-            for (int j = i + 1; j < numbers.size(); j++) {
-                double distance = numbers.get(i) - numbers.get(j);
-                if (distance < threshold) return true;
+public class GenSolution_1 {
+    public List<String> separateParenGroups(String paren_string) {
+        List<String> result = new ArrayList<>();
+        StringBuilder current_string = new StringBuilder();
+        int current_depth = 0;
+
+        for (char c : paren_string.toCharArray()) {
+            if (c == '(') {
+                current_depth += 1;
+                current_string.append(c);
+            } else if (c == ')') {
+                current_depth -= 1;
+                current_string.append(c);
+
+                if (current_depth < 0) {
+                    result.add(current_string.toString());
+                    current_string.setLength(0);
+                }
             }
         }
-        return false;
+        return result;
+
     }
 }
