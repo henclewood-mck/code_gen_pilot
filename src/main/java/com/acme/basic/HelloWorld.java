2aa2d81b2ecd724313358d40e20990437b192cda
Dwayne Henclewood <Dwayne_Henclewood@mckinsey.com>
2023-10-09 22:29:01 -0400

    removed soluiton 0 ref

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -11,19 +11,6 @@ public class HelloWorld {
   void notCovered() {
     System.out.println("This method is not covered by unit tests");
   }
-
-
-
 }
 
-class Solution_0 {
-    public boolean hasCloseElements(List<Double> numbers, double threshold) {
-        for (int i = 0; i < numbers.size(); i++) {
-            for (int j = i + 1; j < numbers.size(); j++) {
-                double distance = Math.abs(numbers.get(i) - numbers.get(j));
-                if (distance < threshold) return true;
-            }
-        }
-        return false;
-    }
-}
+
