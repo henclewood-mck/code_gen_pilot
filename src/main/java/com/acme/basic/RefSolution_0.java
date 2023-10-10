7657af6b060f58889546b505ee20aa2a62ca80fb
Dwayne Henclewood <Dwayne_Henclewood@mckinsey.com>
2023-10-09 22:53:28 -0400

    added bug to  RefSoluiton_0 + vs minus

--- src/main/java/com/acme/basic/RefSolution_0.java
+++ src/main/java/com/acme/basic/RefSolution_0.java
@@ -6,7 +6,7 @@ public class RefSolution_0 {
     public boolean hasCloseElements(List<Double> numbers, double threshold) {
         for (int i = 0; i < numbers.size(); i++) {
             for (int j = i + 1; j < numbers.size(); j++) {
-                double distance = Math.abs(numbers.get(i) - numbers.get(j));
+                double distance = Math.abs(numbers.get(i) + numbers.get(j));
                 if (distance < threshold) return true;
             }
         }
