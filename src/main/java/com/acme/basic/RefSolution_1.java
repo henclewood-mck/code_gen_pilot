163a6c084347973b8445514674de35be76552021
Dwayne Henclewood <Dwayne_Henclewood@mckinsey.com>
2023-10-09 23:17:52 -0400

    added bug to  RefSoluiton_1 and Test

---
+++ src/main/java/com/acme/basic/RefSolution_1.java
@@ -0,0 +1,28 @@
+package com.acme.basic;
+import java.util.*;
+import java.lang.*;
+
+public class RefSolution_1 {
+    public List<String> separateParenGroups(String paren_string) {
+        List<String> result = new ArrayList<>();
+        StringBuilder current_string = new StringBuilder();
+        int current_depth = 0;
+
+        for (char c : paren_string.toCharArray()) {
+            if (c == '(') {
+                current_depth += 1;
+                current_string.append(c);
+            } else if (c == ')') {
+                current_depth -= 1;
+                current_string.append(c);
+
+                if (current_depth == 0) {
+                    result.add(current_string.toString());
+                    current_string.setLength(0);
+                }
+            }
+        }
+        return result;
+
+    }
+}
---
+++ src/test/java/com/acme/basic/RefSolution_1Test.java
@@ -0,0 +1,28 @@
+package com.acme.basic;
+import java.util.*;
+import java.lang.*;
+
+import org.junit.Test;
+
+public class RefSolution_1Test {
+    @Test
+    public void RefSolution_1Test() {
+        RefSolution_1 s = new RefSolution_1();
+        List<Boolean> correct = Arrays.asList(
+                s.separateParenGroups("(()()) ((())) () ((())()())").equals(Arrays.asList(
+                        "(()())", "((()))", "()", "((())()())"
+                )),
+                s.separateParenGroups("() (()) ((())) (((())))").equals(Arrays.asList(
+                        "()", "(())", "((()))", "(((())))"
+                )),
+                s.separateParenGroups("(()(())((())))").equals(Arrays.asList(
+                        "(()(())((())))"
+                )),
+                s.separateParenGroups("( ) (( )) (( )( ))").equals(Arrays.asList("()", "(())", "(()())"))
+        );
+        if (correct.contains(false)) {
+            throw new AssertionError();
+        }
+    }
+    
+}
