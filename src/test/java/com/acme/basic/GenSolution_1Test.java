bb2b993b32eac759dafd1dce31b099f818d36925
Dwayne Henclewood <Dwayne_Henclewood@mckinsey.com>
2023-10-09 23:40:19 -0400

    making genSolution pass

--- src/test/java/com/acme/basic/GenSolution_1Test.java
+++ src/test/java/com/acme/basic/GenSolution_1Test.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class GenSolution_1Test {
     @Test
     public void GenSolution_1Test() {
-        GenSolution_1 s = new GenSolution_1();
+        RefSolution_1 s = new RefSolution_1();
         List<Boolean> correct = Arrays.asList(
                 s.separateParenGroups("(()()) ((())) () ((())()())").equals(Arrays.asList(
                         "(()())", "((()))", "()", "((())()())"
